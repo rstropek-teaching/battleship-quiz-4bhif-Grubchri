49b772bd2cf338cf42ffc9e5166c39eca9b18124
Grubchri <chgr2@gmx.at>
2017-10-04 19:31:33 +0200

    wasn't able to test my program due to some issues with npm not letting it install on my pc

--- battleship.js
+++ battleship.js
@@ -1,21 +1,50 @@
 $(() => {
   // Select table containing the battleground
-  const battleground = $('#battleground');
-
+    const battleground = $('#battleground');
+    var ctr = 0; 
+    var randomleng = 0; //defining ship by length instead of defining a ship.
+    var shipcrossing = false;
   // Build 10 x 10 grid for battleground
-  for (let row = 0; row < 10; row++) {
-    // Create table row
-    const tr = $('<tr>');
-    for (let column = 0; column < 10; column++) {
+    for (let row = 0; row < 10; row++) {
+        // Create table row
+        const tr = $('<tr>');
+        for (let column = 0; column < 10; column++) {
+            if (randomleng === 0) {
+                randomleng = Math.floor((Math.random() * 6));
+                $('<td>').addClass('water').attr('data-r', row).attr('data-c', column).appendTo(tr);
+            } else {
+                ctr = randomleng;
+                if (column + ctr < 10) {
+
+                    for (let key = columns; key < 10 && key < column + ctr; key++) {
+                        if ($('<tr>').hasClass('water')){
+                            shipscrossed = true;
+                            columns = 10;
+                        }
+                    }
+
+                    if (shipscrossed === false) {
+                        $('<td>').addClass('ship').attr('data-r', row).attr('data-c', column).appendTo(tr);
+                        $('<td>').addClass('water').attr('data-r', row).attr('data-c', column+1).appendTo(tr);
+                    }
+
+                    //$('<tr>').hasClass('water');
+                    
+                } else {
+                    $('<td>').addClass('water').attr('data-r', row).attr('data-c', column).appendTo(tr);
+                }
+                ctr -= 1;
+            }
+            
       // Create table cell with CSS class `water`. Note that we use
       // HTML data attributes  to store the coordinates of each cell
       // (see https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes). 
       // That makes it much easier to find cells based on coordinates later.
-      $('<td>').addClass('water').attr('data-r', row).attr('data-c', column).appendTo(tr);
-    }
+        //$('<td>').addClass('water').attr('data-r', row).attr('data-c', column).appendTo(tr);
+        }
 
     // Add table row to battleground table
-    tr.appendTo(battleground);
+        tr.appendTo(battleground);
   }
 
   $('#generate').click(() => {
